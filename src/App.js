67370190e171475cc8f45cf0c9f9151c056ec4db
Fificode <lamidifirdaws@gmail.com>
2022-05-26 23:41:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="w-5 h-6 bg-blue-100">Text</div>
+    </div>
+  );
+}
+
+export default App;
